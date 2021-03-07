d01ec0d16f407e7c0056610b030e09b0b2078616
Pedro Gustavo <pedro.gustavo.santana@outlook.com>
2021-03-07 18:36:27 -0300

    GIPHY project complete

---
+++ project_curse5/README.md
@@ -0,0 +1,3 @@
+# project_curse5
+
+This screen application it's a GIPHY GIF searcherer and sharer.
---
+++ project_curse5/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import 'ui/home_page.dart';
+
+void main(List<String> args) {
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(highlightColor: Colors.white),
+  ));
+}
